c9a80ded32b482959a210b07ad794865815db24a
saucerman <gyq0929@gmail.com>
2018-10-17 22:58:22 +0800

    修复一个错误

--- source_leak/source_leak_check.py
+++ source_leak/source_leak_check.py
@@ -21,7 +21,7 @@ class source_leak_check(threading.Thread):
         while not self.queue.empty():
             url=self.queue.get()
             headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"}
-            for payload in payloads:
+            for payload in self.payloads:
                 vulnurl = url + payload
                 try:
                     req = requests.get(vulnurl, headers=headers, timeout=2, verify=False, allow_redirects=False)
